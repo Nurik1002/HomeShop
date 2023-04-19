fb690c92e62a6b7b50690180428bf652012581e5
Yarashayev Nurgeldi <nyarashbayev97@gmail.com>
2023-04-19 08:16:41 +0500

    Update files

--- home/models.py
+++ home/models.py
@@ -2,5 +2,7 @@ from django.db import models
 from moderator.models import MainUser
 
 class Home(models.Model):
-    user = models.ForeginKey(MainUser,)
-    photo = models.ImageField()
+    user = models.models.ForeignKey(Manufacturer, on_delete=models.CASCADE, verbose_name = 'Egasi')
+    photo = models.ImageField(upload_to = 'images/home', vesrbose_name = 'Rasm')
+    address = models.CharField(max_length = 100, verbose_name='Manzil')
+    num_of_rooms = models.PozitiveSmallIntegerField()
